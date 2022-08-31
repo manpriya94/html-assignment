e7621b95bfeb5898f73a4faaf4f7e6d8bd016380
Priyanka Agrawal <agrawalpriyanka1612@gmail.com>
2022-08-31 16:06:03 +0530

    use for loop to look at all input elements

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 function showshape() {
 
-    let circle = document.getElementById("circle");
-    let square = document.getElementById("square");
-    let triangle = document.getElementById("triangle");
-    let oval = document.getElementById("oval");
-
+    const all_inputs = document.getElementsByName("shape");
     const all_shapes = document.getElementsByClassName("shape");
 
     let shape_id;
 
-    if (circle.checked === true)
-        shape_id = 'circle_shape'
-    else if (square.checked === true)
-        shape_id = 'square_shape'
-    else if (triangle.checked === true)
-        shape_id = 'triangle_shape'
-    else if (oval.checked === true)
-        shape_id = 'oval_shape'
+    for (const input of all_inputs) {
+        if (input.checked === true) {
+            shape_id = input.id + '_shape';
+            break;
+        }
+    }
 
     for (let shape of all_shapes) {
         if (shape.id === shape_id) {
